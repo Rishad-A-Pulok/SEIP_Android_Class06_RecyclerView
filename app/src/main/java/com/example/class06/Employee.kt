03108dfebca87e7e122bc490dd3c16ee5a3b25db
Rishad Amin Pulok <rishad15-2117@diu.edu.bd>
2022-04-05 16:04:59 +0600

    object binding example

---
+++ app/src/main/java/com/example/class06/Employee.kt
@@ -0,0 +1,26 @@
+package com.example.class06
+
+data class Employee(
+    val id: Int,
+    val name: String,
+    val designation: String
+)
+
+val empList = mutableListOf(
+    Employee(1,"abc","def"),
+    Employee(2,"dfbdf","rttwe"),
+    Employee(3,"bdvb","dfher"),
+    Employee(4,"bsgh","fnfdf"),
+    Employee(5,"erwe","fne"),
+    Employee(6,"bbdf","gfn"),
+    Employee(7,"fesf","wenf"),
+    Employee(8,"sferf","werewr"),
+    Employee(9,"bdvs","hner"),
+    Employee(10,"sfdw","dggb"),
+    Employee(11,"tythgb","dgvd"),
+    Employee(12,"rfef","hrerr"),
+    Employee(13,"bbgd","thnet"),
+    Employee(14,"srwwe","hnft"),
+    Employee(15,"dgmj","gddf"),
+    Employee(16,"yjhn","aaree"),
+)
